729864ae311798cd126dd452078b7f8c7129c553
lsamano <leizl.samano@flatironschool.com>
2019-05-08 11:30:13 -0400

    full CRUD on Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,46 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.all
-    @data = = RestClient.get('http://swapi.co/api/people/1')
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.assign_attributes(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :bio)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index]
+  resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
