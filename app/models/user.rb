f2187624c93d09df604deffa40b8d6391be4a2ba
lsamano <leizl.samano@flatironschool.com>
2019-05-10 13:45:24 -0400

    added new validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :commented_posts, through: :comments, source: :post
+
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  validates_presence_of :password_digest
+
 end
